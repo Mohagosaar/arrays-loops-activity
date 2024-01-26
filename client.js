2557b4426d3de121a9487f5dc7e291089c9d77d4
Moha Ali <Moha.gosaar@gmail.com>
2024-01-25 21:14:05 -0600

    done

--- client.js
+++ client.js
@@ -5,8 +5,18 @@
  * 2. Write a loop that logs out each hobby
  * 3. After the loop, log out the total number of
  *    hobbies
+ *
+ *
  */
 
+let hobbies = ["carpentry", "camps", "soccer"];
+
+console.log(hobbies.length);
+console;
+for (let index = 0; index < hobbies.length; index++) {
+  console.log(hobbies[index]);
+}
+console.log("the total number of hobbies", hobbies.length);
 
 // Example output
 // 1. swimming
@@ -20,33 +30,56 @@
  * 1. Create an array of colors as strings
  *    (include the color 'teal' at least once)
  * 2. Create a variable tealCount
- * 3. Write a loop that counts the number of times teal 
+ * 3. Write a loop that counts the number of times teal
  *    is in the array
  * 4. Output the array and number of times teal was found
  */
-
+let colors = ["blue", "yellow", "teal", "violet", "teal", "green"];
+let tealCount;
+for (let index = 0; index < colors.length; index++) {
+  if (colors[index] === "teal") {
+    tealCount = colors.indexOf("teal");
+    console.log(colors[index], "found", tealCount);
+  }
+}
+console.log(colors);
 
 // Example output
 // green, red, teal, orange, teal
 // Teal was found 2 times
-
 
 /**
  * #3 Even & Odd
  * -------------
  * 1. Create an array of numbers (at least 5 numbers)
  * 2. Create variables oddNumbers and evenNumbers (empty arrays)
- * 3. Write a loop that puts all the odd numbers in the oddNumbers 
+ * 3. Write a loop that puts all the odd numbers in the oddNumbers
  *    array and even numbers in the evenNumbers array.
  * 4. Output the original array, odd number array and even number array
  */
-
+let numbers = [10, 15, 7, 4, 9];
+let oddNumbers = [];
+let evenNumbers = [];
+for (let index = 0; index < numbers.length; index++) {
+  if (numbers[index] % 2 == 0) {
+    evenNumbers[index] = numbers[index];
+  } else {
+    oddNumbers[index] = numbers[index];
+  }
+}
+console.log(
+  "The Original Array is",
+  numbers,
+  "Even numbers",
+  evenNumbers,
+  "Odd numbers",
+  oddNumbers
+);
 
 // Example output
 // 3, 7, 2, 8, 11, 4, 2
 // Odd 3, 7, 11
 // Even 2, 8, 4, 2
-
 
 /**
  * #4 Flipping Switches
@@ -57,11 +90,18 @@
  * 4. Output both arrays
  */
 
+let values = [true, false, true, true];
+let toggle = [];
+
+for (let index = 0; index < values.length; index++) {
+  toggle[index] = !values[index];
+}
+console.log("The origin is ", values);
+console.log("Toggle is", toggle);
 
 // Example output
 // true, false, true, true
 // Toggled false, true, false, false
-
 
 /**
  * #5 (STRETCH) Remove 0's
@@ -75,11 +115,21 @@
  * Hint: Try using a while loop for this one.
  */
 
-
 // Example output
 // Before loop 3, 0, 2, 8, 0, 0, 0
 // After loop 3, 0, 2, 8
 
+let removeValues = [3, 0, 2, 8, 0, 0, 0];
+let count = 0;
+
+for (let index = removeValues.length - 1; index > 0; index--) {
+  if (removeValues[index] === 0) {
+    removeValues.pop();
+  } else {
+    break;
+  }
+}
+console.log(removeValues);
 
 /**
  * #6 (STRETCH) Highest & Lowest
@@ -91,8 +141,22 @@
  * 3. Log the highest and lowest number to the console
  */
 
-
 // Example output
 // 2, 2, -3, 7, 4, 1, 7, 12, 8
 // High: 12
 // Low: -3
+
+let maxnumbers = [2, 2, -3, 7, 4, 1, 7, 12, 8];
+let max = maxnumbers[0];
+let min = maxnumbers[0];
+
+for (let index = 0; index < maxnumbers.length; index++) {
+  if (maxnumbers[index] > max) {
+    max = maxnumbers[index];
+  }
+  if (maxnumbers[index] < min) {
+    min = maxnumbers[index];
+  }
+}
+console.log(max);
+console.log(min);
